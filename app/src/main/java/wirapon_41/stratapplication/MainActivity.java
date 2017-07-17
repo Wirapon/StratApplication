154d08806daa643e9762b150b3d1627a501a5632
wirapon <wirapon246@gmail.com>
2017-07-17 20:15:51 +0700

    startProject

---
+++ app/src/main/java/wirapon_41/stratapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package wirapon_41.stratapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+        public void Onclick(View view){
+            Button send = (Button)findViewById(R.id.button);
+            Intent intent = new Intent(MainActivity.this,PageActivity.class);
+            startActivity(intent);
+
+        }
+
+
+    }
+
+
---
+++ app/src/main/java/wirapon_41/stratapplication/PageActivity.java
@@ -0,0 +1,16 @@
+package wirapon_41.stratapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by GGG on 17/7/2560.
+ */
+
+public class PageActivity extends Activity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_activity);
+    }
+}
