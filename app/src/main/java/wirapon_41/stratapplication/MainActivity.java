c9fd96a0fd955525d712824c5adc2f2e91567e6b
wirapon <wirapon246@gmail.com>
2017-07-25 10:38:53 +0700

    startProject

--- app/src/main/java/wirapon_41/stratapplication/MainActivity.java
+++ app/src/main/java/wirapon_41/stratapplication/MainActivity.java
@@ -6,23 +6,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
+public static final String EXTRA_MESSAGE = "com.exaple.myfistapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-        public void Onclick(View view){
-            Button send = (Button)findViewById(R.id.button);
-            Intent intent = new Intent(MainActivity.this,PageActivity.class);
-            startActivity(intent);
+            public void Onclick(View view) {
+               Button btn1 = (Button)findViewById(R.id.button);
+                Intent intent = new Intent(MainActivity.this,PageActivity.class);
+                EditText editText = (EditText) findViewById(R.id.editText);
+                String message = editText.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+}
 
-        }
 
 
-    }
+
 
 
--- app/src/main/java/wirapon_41/stratapplication/PageActivity.java
+++ app/src/main/java/wirapon_41/stratapplication/PageActivity.java
@@ -1,16 +1,29 @@
 package wirapon_41.stratapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by GGG on 17/7/2560.
  */
 
 public class PageActivity extends Activity {
+    TextView textView;
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_activity);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(message);
     }
+
 }
